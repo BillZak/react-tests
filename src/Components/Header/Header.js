721279711ec7ce4c2cec07464ef5cec59fef4df9
Bill Zack <bzak954@gmail.com>
2021-12-05 18:25:22 +0000

    add header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Header/Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <p className="headerName">Instagram</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
