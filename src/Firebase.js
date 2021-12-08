6902bd06c5a658d84bc6f134ff6b11419df70dae
Bill Zack <bzak954@gmail.com>
2021-12-08 19:30:24 +0000

    configure firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,20 +1,36 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
-import "firebase/compat/storage";
+// import firebase from "firebase/compat/app";
 
-const firebaseApp = firebase.initializeApp({
+// const firebaseApp = firebase.initializeApp({
+//   apiKey: "AIzaSyDzOpwXff_bcXW5k316_notjQl8VtctN9s",
+//   authDomain: "instagram-clone-react-ae91d.firebaseapp.com",
+//   projectId: "instagram-clone-react-ae91d",
+//   storageBucket: "instagram-clone-react-ae91d.appspot.com",
+//   messagingSenderId: "832539972056",
+//   appId: "1:832539972056:web:72a8517756777b25e262ad",
+//   //measurementId: "${config.measurementId}",
+// });
+
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+// const storage = firebase.storage();
+
+// export { db, auth, storage };
+
+import firebase from "firebase";
+
+const firebaseConfig = {
   apiKey: "AIzaSyDzOpwXff_bcXW5k316_notjQl8VtctN9s",
   authDomain: "instagram-clone-react-ae91d.firebaseapp.com",
   projectId: "instagram-clone-react-ae91d",
   storageBucket: "instagram-clone-react-ae91d.appspot.com",
   messagingSenderId: "832539972056",
   appId: "1:832539972056:web:72a8517756777b25e262ad",
-  measurementId: "G-82P77PXYRP",
-});
+  measurementId: "${config.measurementId}",
+};
 
-const db = firebaseApp.firestore();
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
 
-export { db, auth, storage };
+export default db;
