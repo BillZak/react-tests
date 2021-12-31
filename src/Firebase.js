190034a9f8ebceeecdb2de0b3883e296b9dadc51
Bill Zack <bzak954@gmail.com>
2021-12-31 17:32:12 +0000

    add firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,4 +1,4 @@
-// import firebase from "firebase/compat/app";
+// import firebase from "firebase";
 
 // const firebaseApp = firebase.initializeApp({
 //   apiKey: "AIzaSyDzOpwXff_bcXW5k316_notjQl8VtctN9s",
@@ -7,17 +7,19 @@
 //   storageBucket: "instagram-clone-react-ae91d.appspot.com",
 //   messagingSenderId: "832539972056",
 //   appId: "1:832539972056:web:72a8517756777b25e262ad",
-//   //measurementId: "${config.measurementId}",
+//   measurementId: "${config.measurementId}",
 // });
 
 // const db = firebaseApp.firestore();
 // const auth = firebase.auth();
 // const storage = firebase.storage();
 
-// export { db, auth, storage };
+// export { auth, storage };
+// export default db;
 
 import firebase from "firebase";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDzOpwXff_bcXW5k316_notjQl8VtctN9s",
   authDomain: "instagram-clone-react-ae91d.firebaseapp.com",
@@ -28,9 +30,9 @@ const firebaseConfig = {
   measurementId: "${config.measurementId}",
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
 const auth = firebase.auth();
-const storage = firebase.storage();
 
+export { auth };
 export default db;
