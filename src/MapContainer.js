0916886f1ef5112673ef8640d869d837ae1523d3
Bill Zack <bzak954@gmail.com>
2022-06-30 21:46:45 +0000

    comment out component

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,21 +1,21 @@
-import { Map, TileLayer } from "react-leaflet";
-import React, { useRef, useState } from "react";
+// import { Map, TileLayer } from "react-leaflet";
+// import React, { useRef, useState } from "react";
 
-const MapContainer = ({ item }) => {
-  const [center, setCenter] = useState({ lat: 13.084322, lng: 80.245388 });
-  const ZOOM_LEVEL = 9;
-  const mapRef = useRef();
+// const MapContainer = ({ item }) => {
+//   const [center, setCenter] = useState({ lat: 13.084322, lng: 80.245388 });
+//   const ZOOM_LEVEL = 9;
+//   const mapRef = useRef();
 
-  return (
-    <div style={{ height: 500 }}>
-      <Map center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
-        <TileLayer
-          url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=QOSShjGGsEubPkLkzCTb"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-      </Map>
-    </div>
-  );
-};
+//   return (
+//     <div style={{ height: 500 }}>
+//       <Map center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
+//         <TileLayer
+//           url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=QOSShjGGsEubPkLkzCTb"
+//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//         />
+//       </Map>
+//     </div>
+//   );
+// };
 
-export default MapContainer;
+// export default MapContainer;
